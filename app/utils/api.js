a7969657d37f1390fa35c9c511690957c26e1594
bxchang04 <bxchang04@gmail.com>
2020-08-14 21:49:51 -0400

    implement project API - rate limited

--- app/utils/api.js
+++ app/utils/api.js
@@ -1,3 +1,68 @@
+const id = "d6ed66107ab0411886bb"
+const sec = "05c84fa0b4cbce8ca6563a3ff7081b0d66027565"
+const params = `?client_id=${id}&client_secret=${sec}`
+
+function getErrorMsg (message, username) {
+  if (message === 'Not Found') {
+    return `${username} doesn't exist`
+  }
+
+  return message
+}
+
+function getProfile (username) {
+  return fetch(`https://api.github.com/users/${username}${params}`)
+    .then((res) => res.json())
+    .then((profile) => {
+      if (profile.message) {
+        throw new Error(getErrorMsg(profile.message, username))
+      }
+
+      return profile
+    })
+}
+
+function getRepos (username) {
+  return fetch(`https://api.github.com/users/${username}/repos${params}&per_page=100`)
+    .then((res) => res.json())
+    .then((repos) => {
+      if (repos.message) {
+        throw new Error(getErrorMsg(repos.message, username))
+      }
+
+      return repos
+    })
+}
+
+function getStarCount (repos) {
+  return repos.reduce((count, {stargazers_count}) => count + stargazers_count , 0)
+}
+
+function calculateScore () {
+  return (followers * 3) + getStarCount(repos)
+}
+
+function getUserData (player) {
+  return Promise.all([
+    getProfile(player),
+    getRepos(player)
+  ]).then(([ profile, repos ]) => ({
+    profile,
+    score: calculateScore(profile.followers, repos)
+  }))
+}
+
+function sortPlayers (players) {
+  return players.sort((a, b) => b.score - a.score)
+}
+
+export function battle (players) {
+  return Promise.all([
+    getUserData(players[0]),
+    getUserData(players[1])
+  ]).then((results) => sortPlayers(results))
+}
+
 export function fetchPopularRepos (language) {
   const endpoint = window.encodeURI(`https://api.github.com/search/repositories?q=stars:>1+language:${language}&sort=stars&order=desc&type=Repositories`)
 
